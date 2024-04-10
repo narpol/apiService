60413e1ca90ead2a285ff0f11e17c844bd6d26de
Felipe Naranjo <felipe.naranjo@ias.com.co>
2024-04-10 14:48:54 -0500

    inicio prueba backend api servicios

---
+++ apiServicios/src/main/java/com/evaluaciontecnica/apiServicios/ApiServiciosApplication.java
@@ -0,0 +1,13 @@
+package com.evaluaciontecnica.apiServicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServiciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServiciosApplication.class, args);
+	}
+
+}
---
+++ apiServicios/src/test/java/com/evaluaciontecnica/apiServicios/ApiServiciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluaciontecnica.apiServicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
